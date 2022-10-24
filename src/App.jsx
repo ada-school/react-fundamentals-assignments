ee1f354a9592923cee771ec88c723fa2bd5599bb
Gabriel Bernal <gbernal@redhat.com>
2022-10-24 18:52:18 +0200

    hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,22 @@
+import { useState } from "react";
+import "./App.css";
+import { getColor } from "./color-service";
+
 export function App() {
-  return <div>Welcome to React</div>;
+  const [counter, setCounter] = useState(0);
+  const [color, setColor] = useState("red");
+
+  return (
+    <div>
+      <h1>Ada School - React Hooks</h1>
+
+      <div>
+        <span className={color}>Counter:{counter}</span>
+      </div>
+
+      <button>Increase</button>
+      <button>Decrease</button>
+      <button>Reset</button>
+    </div>
+  );
 }
---
+++ src/color-service.jsx
@@ -0,0 +1,3 @@
+export const getColor = () => {
+  return Promise.resolve("blue");
+};
